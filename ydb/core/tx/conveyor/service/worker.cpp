5bc65ca857f88bae4a98b02ec9ac6523f537cb2b
zverevgeny <zverevgeny@ydb.tech>
2024-07-01 11:07:16 +0300

    Olap perf to 24-2 (#5950)
    Co-authored-by: ivanmorozov333 <ivanmorozov@ydb.tech>

--- ydb/core/tx/conveyor/service/worker.cpp
+++ ydb/core/tx/conveyor/service/worker.cpp
@@ -29,6 +29,26 @@ void TWorker::HandleMain(TEvInternal::TEvNewTask::TPtr& ev) {
     } else {
         WaitTask = ev->Get()->GetTask();
     }
+    if (CPUUsage < 1) {
+        Schedule((TMonotonic::Now() - *start) * (1 - CPUUsage), new NActors::TEvents::TEvWakeup);
+        WaitWakeUp = true;
+    }
+}
+
+void TWorker::HandleMain(NActors::TEvents::TEvWakeup::TPtr& /*ev*/) {
+    WaitWakeUp = false;
+    if (WaitTask) {
+        ExecuteTask(*WaitTask);
+        WaitTask.reset();
+    }
+}
+
+void TWorker::HandleMain(TEvInternal::TEvNewTask::TPtr& ev) {
+    if (!WaitWakeUp) {
+        ExecuteTask(ev->Get()->GetTask());
+    } else {
+        WaitTask = ev->Get()->GetTask();
+    }
 }
 
 }
